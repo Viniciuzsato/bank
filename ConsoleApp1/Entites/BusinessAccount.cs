a93a42491ca8b63cb2064a03bc9666fd0e0b1117
vinicius sato <vinicius.sato@outlook.com>
2022-10-06 21:33:16 -0300

    Primeiro commit

---
+++ ConsoleApp1/Entites/BusinessAccount.cs
@@ -0,0 +1,25 @@
+﻿namespace Course.Entites
+{
+    internal class BusinessAccount : Account
+    {
+        public double LoanLimit { get; set; }
+        
+        public BusinessAccount()
+        {
+        }
+
+        public BusinessAccount(int number, string holder, double balance, double LoanLimit) : base(number, holder, balance)
+        {
+            LoanLimit = LoanLimit;
+        }
+
+        public void Loan(double amount)
+        {
+            if(amount <= LoanLimit)
+            {
+                Balance += amount;
+            }
+        }
+    }
+
+}
---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Course.Entites;
+    
+
+namespace Course
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            BusinessAccount account = new(0446, "Vinicius Sato", 1000.0, 500);
+
+            Console.WriteLine(account.Balance);
+            
+        }
+    }
+}
