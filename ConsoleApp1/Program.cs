5cceaac22fef2c535c591cdcb193c16fa215785c
vinicius sato <vinicius.sato@outlook.com>
2022-10-15 14:08:14 -0300

    adicionando funcionalidade saque

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,6 +1,6 @@
 ﻿using System;
 using Course.Entites;
-    
+using Transactions.Entites;
 
 namespace Course
 {
@@ -8,10 +8,14 @@ namespace Course
     {
         static void Main(string[] args)
         {
-            BusinessAccount account = new(0446, "Vinicius Sato", 1000.0, 500);
+            Account acc1 = new Account(0446, "Vinicius Sato", 500.0);
+            Account acc2 = new SavingsAccount(1008, "Natalia", 500.0, 0.01);
 
-            Console.WriteLine(account.Balance);
-            
+            acc1.Withdraw(10.0);
+            acc2.Withdraw(10.0);
+
+            Console.WriteLine(acc1.Balance);
+            Console.WriteLine(acc2.Balance);
         }
     }
 }
